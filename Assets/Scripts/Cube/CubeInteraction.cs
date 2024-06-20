dde0f84360b034af4650abecae3439e0fa068a6b
leonnik <lencruseckij@gmail.com>
2024-06-20 21:56:38 +0300

    Create Cube Interaction System

---
+++ Assets/Scripts/Cube/CubeInteraction.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour
+{
+    [SerializeField] private float _raycastDistance = 5f;
+
+    private GameObject _selectedCube;
+    private bool _isHoldingCube;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (_isHoldingCube)
+            {
+                ReleaseCube();
+            }
+            else
+            {
+                PickUpCube();
+            }
+        }
+    }
+
+    private void ReleaseCube()
+    {
+        _selectedCube.GetComponent<Rigidbody>().isKinematic = false;
+        _selectedCube.transform.SetParent(null);
+        _selectedCube = null;
+        _isHoldingCube = false;
+    }
+
+    private void PickUpCube()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _raycastDistance))
+        {
+            if (hit.transform.TryGetComponent(out ICube _))
+            {
+                _selectedCube = hit.collider.gameObject;
+                _selectedCube.GetComponent<Rigidbody>().isKinematic = true;
+                _selectedCube.transform.SetParent(transform);
+                _isHoldingCube = true;
+            }
+        }
+    }
+}
